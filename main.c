d4d9ad29e6f46c1adb3e721c4557b68b86e55a8f
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-03-23 04:35:45 -0700

    quick-fix

--- main.c
+++ main.c
@@ -1,13 +1,13 @@
 #include "monty.h"
 #include <stdio.h>
-
 bus_t bus = {NULL, NULL, NULL, 0};
+
 /**
-* main - monty code interpreter
-* @argc: number of arguments
-* @argv: monty file location
-* Return: 0 on success
-*/
+ * main - monty code interpreter
+ * @argc: number of arguments
+ * @argv: monty file location
+ * Return: 0 on success
+ */
 int main(int argc, char *argv[])
 {
 	char *content;
@@ -43,5 +43,5 @@ int main(int argc, char *argv[])
 	}
 	free_stack(stack);
 	fclose(file);
-return (0);
+	return (0);
 }
