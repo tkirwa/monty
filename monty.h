247aa150b1d31a419216b6fb47042fa625aeabe9
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-03-23 05:15:05 -0700

    changes

--- monty.h
+++ monty.h
@@ -7,6 +7,8 @@
 #include <fcntl.h>
 #include <string.h>
 #include <ctype.h>
+#define _POSIX_C_SOURCE 200809L
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
