802e6607c99730e6e986923726dd80b6451f1ede
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-03-23 13:48:16 -0700

    fixed

--- main.c
+++ main.c
@@ -1,47 +1,55 @@
 #include "monty.h"
+#define _POSIX_C_SOURCE 200809L
+#include <stdio.h>
+#include <stdlib.h>
 
-/* global struct to hold flag for queue and stack length */
-var_t var;
+bus_t bus = {NULL, NULL, NULL, 0};
 
 /**
- * main - Monty bytecode interpreter
- * @argc: number of arguments passed
- * @argv: array of argument strings
- *
- * Return: EXIT_SUCCESS on success or EXIT_FAILURE on failure
+ * main - monty code interpreter
+ * @argc: number of arguments
+ * @argv: monty file location
+ * Return: 0 on success
  */
 int main(int argc, char *argv[])
 {
-	stack_t *stack = NULL;
-	unsigned int line_number = 0;
-	FILE *fs = NULL;
-	char *lineptr = NULL, *op = NULL;
-	size_t n = 0;
+    char *content;
+    FILE *file;
+    size_t size = 0;
+    ssize_t read_line = 1;
+    stack_t *stack = NULL;
+    unsigned int counter = 0;
 
-	var.queue = 0;
-	var.stack_len = 0;
-	if (argc != 2)
-	{
-		dprintf(STDOUT_FILENO, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
-	fs = fopen(argv[1], "r");
-	if (fs == NULL)
-	{
-		dprintf(STDOUT_FILENO, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
-	on_exit(free_lineptr, &lineptr);
-	on_exit(free_stack, &stack);
-	on_exit(m_fs_close, fs);
-	while (getline(&lineptr, &n, fs) != -1)
-	{
-		line_number++;
-		op = strtok(lineptr, "\n\t\r ");
-		if (op != NULL && op[0] != '#')
-		{
-			get_op(op, &stack, line_number);
-		}
-	}
-	exit(EXIT_SUCCESS);
+    if (argc != 2)
+    {
+        fprintf(stderr, "USAGE: monty file\n");
+        exit(EXIT_FAILURE);
+    }
+
+    file = fopen(argv[1], "r");
+    bus.file = file;
+    if (!file)
+    {
+        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+
+    while (read_line > 0)
+    {
+        content = NULL;
+        read_line = getline(&content, &size, file);
+        bus.content = content;
+        counter++;
+
+        if (read_line > 0)
+        {
+            execute(content, &stack, counter, file);
+        }
+
+        free(content);
+    }
+
+    free_stack(stack);
+    fclose(file);
+    return (0);
 }
