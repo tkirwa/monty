e34ce6f75ffaf0c35e4a70c4c90565085329109e
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-03-23 02:07:58 -0700

    0. push, pall

--- push.c
+++ push.c
@@ -1,24 +1,5 @@
 #include "monty.h"
-/**
- * f_pall - prints the stack
- * @head: stack head
- * @counter: no used
- * Return: no return
-*/
-void f_pall(stack_t **head, unsigned int counter)
-{
-	stack_t *h;
-	(void)counter;
-
-	h = *head;
-	if (h == NULL)
-		return;
-	while (h)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-	}
-}
+#include <stdio.h>
 
 /**
  * f_push - add node to the stack
